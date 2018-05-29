260b41949d929ec1629009daf65f0d0cedc5099f
Martin Fors <martin.fors@frostkom.se>
2018-05-29 12:00:45 +0200

    Hide empty container dashed line

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Custom Dashboard
 Plugin URI: https://github.com/frostkom/mf_custom_dashboard
 Description: 
-Version: 3.3.12
+Version: 3.3.13
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se

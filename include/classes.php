733c361cbc1f1da511270e5cd82541e30d8c4ccd
Martin Fors <martin.fors@frostkom.se>
2025-01-29 09:16:58 +0100

    Remove plugin version

--- include/classes.php
+++ include/classes.php
@@ -121,7 +121,6 @@ class mf_custom_dashboard
 		if($pagenow == 'index.php')
 		{
 			$plugin_include_url = plugin_dir_url(__FILE__);
-			$plugin_version = get_plugin_version(__FILE__);
 
 			$user_data = get_userdata(get_current_user_id());
 			$user_display_name = $user_data->first_name != '' ? $user_data->first_name : ucfirst($user_data->display_name);
@@ -135,8 +134,8 @@ class mf_custom_dashboard
 				$setting_panel_heading = str_replace("[name]", $user_display_name, $setting_panel_heading);
 			}
 
-			mf_enqueue_style('style_custom_dashboard', $plugin_include_url."style_wp.css", $plugin_version);
-			mf_enqueue_script('script_custom_dashboard', $plugin_include_url."script_wp.js", array('panel_heading' => $setting_panel_heading, 'remove_widgets' => $setting_remove_widgets, 'hide_empty_containers' => $setting_panel_hide_empty_containers), $plugin_version);
+			mf_enqueue_style('style_custom_dashboard', $plugin_include_url."style_wp.css");
+			mf_enqueue_script('script_custom_dashboard', $plugin_include_url."script_wp.js", array('panel_heading' => $setting_panel_heading, 'remove_widgets' => $setting_remove_widgets, 'hide_empty_containers' => $setting_panel_hide_empty_containers));
 		}
 	}
 
